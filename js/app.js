1a574801ad9fbfbac65392c24f386ece89f73c29
johnreyalan@gmail.com <johnreyalan001@gmail.com>
2021-10-25 14:32:29 +0700

    Insert toggleBtn for hide and show button

--- js/app.js
+++ js/app.js
@@ -9,7 +9,18 @@ new Vue({
 
     },
     methods:{
+        listTasks() {
+            this.lists.push(this.userInput);
+        },
+        toggleBtn(){
+            this.btn = !this.btn
 
+            if(this.btn === true) {
+                this.toggle = 'Show List'
+            }
+            else{
+                this.toggle = 'Hide'
+            }
          },
     }
 })
